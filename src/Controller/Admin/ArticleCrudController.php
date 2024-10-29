23808b08bf51cfaf40393f2ea745dd3e19cc3952
ramanantsoa01 <safidyramanantsoa@gmail.com>
2024-10-29 08:42:57 +0300

    Installation EasyAdmin

---
+++ src/Controller/Admin/ArticleCrudController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Article;
+use DateTime;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class ArticleCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Article::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('name'),
+            TextEditorField::new('content'),
+            DateTimeField::new('createdAt')
+                ->hideOnForm()
+                ->setFormTypeOption('disabled', true)
+        ];
+    }
+    
+}
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -26,6 +26,11 @@ class Article
     #[ORM\Column]
     private ?\DateTimeImmutable $createdAt = null;
 
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+    
     public function getId(): ?int
     {
         return $this->id;
