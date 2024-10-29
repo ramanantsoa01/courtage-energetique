43f652b90f8cedf983577efe9a7691def1c5032e
ramanantsoa01 <safidyramanantsoa@gmail.com>
2024-10-29 21:08:34 +0300

    Articles dans la page d'accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        $articles = $articleRepository->findBy([], ['createdAt' => 'DESC'], 3);
+
+        return $this->render('home/index.html.twig', [
+            'articles' => $articles,
+        ]);
     }
 }
